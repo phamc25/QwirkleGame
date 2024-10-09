1b8f8b2096e4c0605da005d561b7e0678fbc93c5
Chloe Pham <chloepham03@gmail.com>
2024-10-09 02:18:35 -0700

    Wrote some code for actions

--- app/src/main/java/edu/up/cs301/qwirklegame/QwirkleTiles.java
+++ app/src/main/java/edu/up/cs301/qwirklegame/QwirkleTiles.java
@@ -1,10 +1,15 @@
 package edu.up.cs301.qwirklegame;
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.shapes.Shape;
 
-import android.graphics.Bitmap;
+/**
+ * This contains the object for Qwirkle Tiles
+ *
+ * @author Talia Martinez
+ * @author Chloe Pham
+ *
+ * @version October 5, 2024
+ */
 
 public class QwirkleTiles {
     // Declaring for each shape of tile
@@ -13,7 +18,8 @@ public class QwirkleTiles {
     }
     private Shape shape;
     private Color color;
-    private Bitmap bitMap;
+    private Bitmap tileImage;
+    private boolean isSelected;
 
     // For each color of tile
     public enum Color {
@@ -21,24 +27,32 @@ public class QwirkleTiles {
     }
 
     // Constructor
-    public QwirkleTiles (Shape shape, Color color) {
+    public QwirkleTiles (Shape shape, Color color, Bitmap image) {
         this.shape = shape;
         this.color = color;
+        this.isSelected = false;
+        this.tileImage = image;
     }
 
     // Copy constructor for tiles
     public QwirkleTiles(QwirkleTiles orig) {
         this.shape = orig.shape;
         this.color = orig.color;
-        this.bitMap = Bitmap.createBitmap(orig.bitMap);
+        this.isSelected = orig.isSelected;
+        this.tileImage = Bitmap.createBitmap(orig.tileImage);
     }
 
+    // Getters for tile attributes
     public Shape getShape() {
         return shape;
     }
     public Color getColor() {
         return color;
     }
+    public boolean getSelected() { return isSelected; }
+
+    // Setters for tile variables
+    public void setSelected(boolean selected) { this.isSelected = selected; }
 
     public boolean isValidTile(QwirkleTiles tile) {
         return true;
