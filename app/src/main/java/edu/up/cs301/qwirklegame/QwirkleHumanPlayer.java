2f6108a7c619c3a0db1e521f6e1fdd18f445f5ff
deanteagleham <agleham27@up.edu>
2024-10-28 20:05:33 -0700

    Player 1 moves

--- app/src/main/java/edu/up/cs301/qwirklegame/QwirkleHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/qwirklegame/QwirkleHumanPlayer.java
@@ -133,6 +133,18 @@ public class QwirkleHumanPlayer extends GameHumanPlayer implements OnClickListen
 		testResultsTextView.append("Player 0 ended their turn and got 2 points. A RED EIGHTSTAR was added to their hand \n");
 
 		// Player 1 turn start
+		QwirkleTile orangeFStar = hand1.get(2);
+		QwirkleTile orangeDiamond = hand1.get(4);
+		QwirkleTile orangeCircle = hand1.get(5);
+		firstInstance.placeTile(new PlaceTileAction(this, orangeFStar,2,1));
+		testResultsTextView.append("Player 1 placed a ORANGE FOURSTAR tile at 2,1 \n");
+		firstInstance.placeTile(new PlaceTileAction(this, orangeDiamond,3,1));
+		testResultsTextView.append("Player 1 placed a ORANGE DIAMOND tile at 3,1 \n");
+		firstInstance.placeTile(new PlaceTileAction(this, orangeCircle,0,1));
+		testResultsTextView.append("Player 1 placed a ORANGE CIRCLE tile at 0,1 \n");
+		playersScore[firstInstance.getCurrPlayer()] += 3;
+		testResultsTextView.append("Player 1 ended their turn and got 3 points. A NO TILES were added to their hand \n");
+
 
 
 
