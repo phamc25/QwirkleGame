f66170f7f2838108072ae0677f3238e8ca41c054
deanteagleham <agleham27@up.edu>
2024-10-28 20:55:05 -0700

    Added extra line to TextView

--- app/src/main/java/edu/up/cs301/qwirklegame/QwirkleHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/qwirklegame/QwirkleHumanPlayer.java
@@ -184,7 +184,7 @@ public class QwirkleHumanPlayer extends GameHumanPlayer implements OnClickListen
 		playersScore[firstInstance.getCurrPlayer()] += 1;
 		firstInstance.endTurn(new EndTurnAction(firstInstance, this, 2));
 		testResultsTextView.append("Player 1 ended their turn and got 1 point. A NO TILES were added to their hand \n");
-		testResultsTextView.append("Player 1 has no more tiles, the game ends.");
+		testResultsTextView.append("Player 1 has no more tiles, the game ends.\n");
 
 		testResultsTextView.append("\n");
 
