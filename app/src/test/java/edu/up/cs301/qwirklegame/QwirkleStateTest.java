779be492aecc281ae1a64c5033ef7016540e5182
Talia <martinet27@up.edu>
2024-11-07 14:16:57 -0800

    Tests from lab

---
+++ app/src/test/java/edu/up/cs301/qwirklegame/QwirkleStateTest.java
@@ -0,0 +1,45 @@
+package edu.up.cs301.qwirklegame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QwirkleStateTest {
+
+    @Test
+    public void getPlayersScore() {
+
+    }
+
+    @Test
+    public void getAddPoints() {
+    }
+
+    @Test
+    public void getCurrPlayer() {
+    }
+
+    @Test
+    public void setAddPoints() {
+    }
+
+    @Test
+    public void setCurrPlayer() {
+    }
+
+    @Test
+    public void setCurrTile() {
+    }
+
+    @Test
+    public void setPlayersScore() {
+    }
+
+    @Test
+    public void refillHand() {
+    }
+
+    @Test
+    public void getPlayerHand() {
+    }
+}
