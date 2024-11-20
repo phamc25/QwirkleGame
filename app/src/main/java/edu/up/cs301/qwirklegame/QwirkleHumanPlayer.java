9e741c1a1e0686f51e16bbb15d562cc7d686e803
phamc25 <phamc25@up.edu>
2024-11-19 17:45:53 -0800

    Added players on XML and

--- app/src/main/java/edu/up/cs301/qwirklegame/QwirkleHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/qwirklegame/QwirkleHumanPlayer.java
@@ -105,6 +105,8 @@ public class QwirkleHumanPlayer extends GameHumanPlayer implements OnClickListen
 		if (state.getCurrPlayer() == this.playerNum) {
 			playerScore.setText(String.valueOf(state.getPlayersScore()[state.getCurrPlayer()]));
 			player2Score.setText("Player 2: " + state.getPlayersScore()[(state.getCurrPlayer() + 1) % state.getPlayersScore().length]);
+//			player3Score.setText("Player 3: " + state.getPlayersScore()[(state.getCurrPlayer() + 2) % state.getPlayersScore().length]);
+//			player4Score.setText("Player 4: " + state.getPlayersScore()[(state.getCurrPlayer() + 3) % state.getPlayersScore().length]);
 		}
 	}
 
@@ -201,12 +203,11 @@ public class QwirkleHumanPlayer extends GameHumanPlayer implements OnClickListen
 		for (int i = 0; i < state.getPlayersScore().length; i++) {
 			System.out.println("Scores:" + state.getPlayersScore()[i]);
 		}
-//			playerScore.setText(String.valueOf(state.getPlayersScore()[state.getCurrPlayer()]));
+		playerScore.setText(String.valueOf(state.getPlayersScore()[state.getCurrPlayer()]));
 
 		updateDisplay();
 		updateHandDisplay();
 		qwirkleView.updateFromGameState(state.getBoard());
-
 	}
 	
 	/**
@@ -241,6 +242,8 @@ public class QwirkleHumanPlayer extends GameHumanPlayer implements OnClickListen
 		this.playerTurn = (TextView) activity.findViewById(R.id.player_turn);
 		this.playerScore = (TextView) activity.findViewById(R.id.player_score);
 		this.player2Score = (TextView) activity.findViewById(R.id.player2);
+		this.player3Score = (TextView) activity.findViewById(R.id.player3);
+		this.player4Score = (TextView) activity.findViewById(R.id.player4);
 
 		// Get the QwirkleView instance and set the touch listener
 		qwirkleView = (QwirkleView) activity.findViewById(R.id.boardView);
